5c0d07ba15ee82d2acf3e5201ad65b6c800b7b3d
Kristofers Solo <kristians.cagulis@pm.me>
2024-01-09 20:29:16 +0200

    feat(ai): add `complete_lines`

--- src/ai/heuristics/lines.py
+++ src/ai/heuristics/lines.py
@@ -2,4 +2,13 @@ import numpy as np
 
 
 def complete_lines(field: np.ndarray[int, np.dtype[np.uint8]]) -> int:
-    return 0
+    """
+    Calculates the number of complete lines in the field.
+
+    Args:
+        field: 2D array representing the game field.
+
+    Returns:
+        The number of complete lines in the field.
+    """
+    return int(np.sum(np.all(field, axis=1)))
